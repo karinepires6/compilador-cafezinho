b6f04fee956171eea38451e75eec3878b3d49217
Karine Araujo <pires.karine6@gmail.com>
2019-11-04 16:10:34 -0300

    implementacao

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -15,6 +15,42 @@ palavras_reservadas = [
     'execute'
 ]
 
+t_operadores = [
+    '+',
+    '-',
+    '*',
+    '/',
+    '%',
+    '==',
+    '!=',
+    '>=',
+    '<=',
+    '<',
+    '>',
+    '!',
+    '?',
+    ':',
+    'e',
+    'ou'
+]
+
+t_delimitadores = [
+    '(',
+    ')',
+    '[',
+    ']',
+    '{',
+    '}',
+    ';',
+    ','
+]
+
+t_terminais = [
+    'id',
+    'carconst',
+    'intconst'
+]
+
 # ignorar tabulação
 t_ignore = " \t"
 
@@ -27,11 +63,32 @@ def t_palavraReservada(palavra):
         if reservada == palavra:
             return True
 
+def t_operador(palavra):
+    for i, operador in t_operadores:
+        if operador == palavra:
+            return t_operadores[i]
+
+def t_delimitador(palavra):
+    for i, deli in t_delimitadores:
+        if deli == palavra:
+            return t_delimitadores[i]
+
+def t_terminal(palavra):
+    for i, ter in t_terminais:
+        if ter == palavra:
+            return t_terminais[i]
+
 def t_id(palavra):
     if re.fullmatch("[a-zA-Z]+\w*", palavra):
         return palavra
     else:
         return None
+
+def verificaComentario(palavra):
+    if re.fullmatch("//*/w+*/"):
+        return True
+    else:
+        return False
 
 def palavraReservadaOuIdentificador(palavra):
     if t_palavraReservada(palavra):
