ff1a0a6cf50e7364233eaf23b3c9141b314958d3
Karine Araujo <pires.karine6@gmail.com>
2019-11-10 14:23:00 -0300

    implementando a leitura de arquivos

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -48,8 +48,7 @@ t_delimitadores = [
 
 t_terminais = [
     'carconst',
-    'intconst',
-
+    'intconst'
 ]
 
 
@@ -84,15 +83,18 @@ def t_id(palavra):
     else:
         return None
 
-#Verifica se a string passada possui comentario
-#Retorna uma lista das posições de inicio e fim dos comentarios
+'''Verifica se a string passada possui comentario
+Retorna uma lista das posições de inicio e fim dos comentarios
 def t_comentario(palavra):
     p = re.compile('\/\*(\*(?!\/)|[^*])*\*\/')
     iterator = p.finditer(palavra)
     listValues = []
     for match in iterator:
-        listValues.append(match.span())
-    return True, listValues
+        listValues.append(match.group(0))
+    return listValues
+'''
+def removeComentarios(palavra):
+    return re.sub('\/\*(\*(?!\/)|[^*])*\*\/', '', palavra)
 
 #Verifica se o token é um integer
 def t_integer(palavra):
@@ -112,39 +114,40 @@ def t_float(palavra):
 def analisador(palavra):
     if t_palavraReservada(palavra):
         return print("Palavra Reservada: {}".format(palavra))
-    elif(t_id(palavra)):
+    elif t_id(palavra):
         return print("Identificador: {}".format(t_id(palavra)))
-    elif(t_operador(palavra)):
+    elif t_operador(palavra):
         return print("Operador: {}".format(palavra))
-    elif(t_comentario(palavra)):
-        return print("Comentário: {}".format(palavra))
-    elif(t_delimitador(palavra)):
+    elif palavra == '':
+        return
+    elif t_delimitador(palavra):
         return print("Delimitador: {}".format(palavra))
-    elif(t_integer(palavra)):
+    elif t_integer(palavra):
         return print("Integer: {}".format(palavra))
-    elif(t_float(palavra)):
+    elif t_float(palavra):
         return print("Float: {}".format(palavra))
     else:
         return print("Terminal: {}".format(palavra))
 
-#text = input("Digite: ")
+def main():
+    #Abre arquivo com o codigo
+    filepath = 'guru99.txt'
+    with open(filepath) as fp:
+        line = fp.readline()
+        cnt = 1
+        #Lê linha por linha no arquivo e separa os tokens
+        while line:
+            #print("Line {}: {}".format(cnt, line.strip()))
+            #antes de fazer o split do texto, verificar se o mesmo possui comentarios
+            newText = removeComentarios(line)
+            splitted_text = re.split("\s", newText)
+            #para cada token verifica qual a qual classe de lexema ele esta inserido
+            for word in splitted_text:
+                analisador(word)
+            line = fp.readline()
+            cnt += 1
 
-##text = "int id , id programa { retorne id = id }"
-#text = "/*karine comentario*/"
-text = "/*aqui tem comentario*/ int id , id programa { retorne id = id } /*outro comentario*/"
-
-#antes de fazer o split do texto, verificar se o mesmo possui comentarios
-found, arrVlr = t_comentario(text)
-if found:
-    for tupleVlr in arrVlr:
-        print('comment ending in {}'.format(tupleVlr[1]))
-
-'''splitted_text = re.split("\s", text)
-print(splitted_text)
-
-for word in splitted_text:
-    analisador(word)'''
-
-
+if __name__== "__main__":
+  main()
 
 
